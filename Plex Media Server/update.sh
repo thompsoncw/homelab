9449dd97c7e22347dd2c3ce8812395585dcf5bd0
thompsoncw <157969073+thompsoncw@users.noreply.github.com>
2024-12-13 14:32:34 -0500

    Update update.sh

--- Plex Media Server/update.sh
+++ Plex Media Server/update.sh
@@ -2,21 +2,30 @@
 
 # Capture the version number of the update
 echo "We just need the version number of the new Plex Media Server release."
-echo "Do not include the 'v.' in the input, as this isn't used in the file name."
+echo "Include the 'v.' in the input if it is present in the version."
 sleep 1
-echo "Example: '1.40.0.7998-c29d4c0c8' not 'v.1.40.0.7998-c29d4c0c8'"
+echo "Example: 'v.1.40.0.7998-c29d4c0c8' or '1.40.0.7998-c29d4c0c8'"
 echo -en "Enter the version number of the server update:"
 read versionNumber
+
+# Check if 'v.' prefix is present and remove it
+if [[ $versionNumber == v.* ]]; then
+    cleanVersion=${versionNumber#v.}
+else
+    cleanVersion=$versionNumber
+fi
+
 # Read back the input to verify
 echo "You entered $versionNumber for the new Plex server version."
-echo "Downloading v.$versionNumber of Plex Server for x86 Linux from https://downloads.plex.tv/plex-media-server-new/${versionNumber}/debian/plexmediaserver_${versionNumber}_amd64.deb"
+echo "Downloading ${versionNumber} of Plex Server for x86 Linux from https://downloads.plex.tv/plex-media-server-new/${cleanVersion}/debian/plexmediaserver_${cleanVersion}_amd64.deb"
 echo "-----------------------------------------------------"
 sleep 1
-# Download the the installer, run it and then delete it
-wget "https://downloads.plex.tv/plex-media-server-new/${versionNumber}/debian/plexmediaserver_${versionNumber}_amd64.deb"
-echo "Running plexmediaserver_"${versionNumber}"_amd46.deb"
-sudo dpkg -i plexmediaserver_${versionNumber}_amd64.deb
+
+# Download the installer, run it and then delete it
+wget "https://downloads.plex.tv/plex-media-server-new/${cleanVersion}/debian/plexmediaserver_${cleanVersion}_amd64.deb"
+echo "Running plexmediaserver_${cleanVersion}_amd64.deb"
+sudo dpkg -i plexmediaserver_${cleanVersion}_amd64.deb
 echo "-----------------------------------------------------"
 sleep 1
-echo "No longer need the installer, so removing plexmediaserver_"${versionNumber}"_amd46.deb"
-sudo rm plexmediaserver_${versionNumber}_amd64.deb
+echo "No longer need the installer, so removing plexmediaserver_${cleanVersion}_amd64.deb"
+sudo rm plexmediaserver_${cleanVersion}_amd64.deb
